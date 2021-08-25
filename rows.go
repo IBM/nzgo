6e5418477a87b3536642cc0dcc60d202acd465a5
prajwapa <prajwapa@in.ibm.com>
2021-08-24 22:57:35 -0700

    Changed text and bytea type length to -1

--- rows.go
+++ rows.go
@@ -48,7 +48,7 @@ func (fd fieldDesc) Name() string {
 func (fd fieldDesc) Length() (length int64, ok bool) {
 	switch fd.OID {
 	case oid.T_text, oid.T_bytea:
-		return 4096, true
+		return -1, true
 	case oid.T_varchar, oid.T_bpchar, oid.T_nvarchar, oid.T_nchar:
 		return int64(fd.Mod - headerSize), true
 	case oid.T_name:
