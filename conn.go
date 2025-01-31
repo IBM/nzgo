b7225325a1c294531107dfc652fb6c3ec225b3ce
Raj-LNU <RAJ.LNU@ibm.com>
2025-01-31 09:27:15 -0800

    segv fix

--- conn.go
+++ conn.go
@@ -11,7 +11,6 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"github.com/golang-jwt/jwt/v5"
 	"io"
 	"math"
 	"net"
@@ -25,6 +24,8 @@ import (
 	"time"
 	"unicode"
 	"unsafe"
+
+	"github.com/golang-jwt/jwt/v5"
 
 	"github.com/IBM/nzgo/v12/oid"
 )
@@ -265,7 +266,7 @@ const (
 	PG_PROTOCOL_5
 )
 
-//Client Type
+// Client Type
 const (
 	NPSCLIENT_TYPE_GOLANG = 12
 )
@@ -3362,7 +3363,8 @@ func (cn *conn) Conn_send_database(o values) (bool, error) {
 	return false, nil
 }
 
-/*Cases which will fail:
+/*
+Cases which will fail:
 Client-> Preferred secured; Server-> Only Unsecured
 Client-> Preferred Unsecured; Server-> Only Secured
 All other cases of client and server combination will be taken care of.
@@ -3434,7 +3436,10 @@ func (cn *conn) Conn_secure_session() (bool, error) {
 				 *   only secured sessions.
 				 */
 				upgrade, err = ssl(cn.opts)
-				if err == nil {
+				if upgrade == nil && err == nil {
+					elog.Infoln(chopPath(funName()), "Host expecting ssl connection but sslmode set to disable")
+					return false, elog.Fatalf(chopPath(funName()), "ERROR_CONN_FAIL")
+				} else if err == nil {
 					information = HSV2_SSL_CONNECT
 
 				} else {
@@ -4064,10 +4069,10 @@ func (rs *rows) NextResultSet() error {
 // QuoteIdentifier quotes an "identifier" (e.g. a table or a column name) to be
 // used as part of an SQL statement.  For example:
 //
-//    tblname := "my_table"
-//    data := "my_data"
-//    quoted := pq.QuoteIdentifier(tblname)
-//    err := db.Exec(fmt.Sprintf("INSERT INTO %s VALUES ($1)", quoted), data)
+//	tblname := "my_table"
+//	data := "my_data"
+//	quoted := pq.QuoteIdentifier(tblname)
+//	err := db.Exec(fmt.Sprintf("INSERT INTO %s VALUES ($1)", quoted), data)
 //
 // Any double quotes in name will be escaped.  The quoted identifier will be
 // case sensitive when used in a query.  If the input string contains a zero
